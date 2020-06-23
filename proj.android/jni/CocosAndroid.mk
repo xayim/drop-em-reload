76bc7a4a2ca342fb380ed3f20cc648f304225947
Zaim Abbasi <xayim@yahoo.com>
2020-06-23 12:23:48 +0500

    Add frameworks SDKBOX PluginAds and setup SDKBOX PluginShare

--- proj.android/jni/CocosAndroid.mk
+++ proj.android/jni/CocosAndroid.mk
@@ -1,10 +1,16 @@
 LOCAL_CPPFLAGS := -DSDKBOX_ENABLED \
 -DSDKBOX_COCOS_CREATOR
 LOCAL_WHOLE_STATIC_LIBRARIES := PluginReview \
-sdkbox
+sdkbox \
+PluginSdkboxAds
 LOCAL_WHOLE_STATIC_LIBRARIES := 
 $(call import-add-path,$(LOCAL_PATH))
 $(call import-module, ./sdkbox)
 $(call import-module, ./PluginReview)
+$(call import-module, ./PluginSdkboxAds)
 LOCAL_LDLIBS := -landroid \
 -llog
+LOCAL_WHOLE_STATIC_LIBRARIES := 
+LOCAL_WHOLE_STATIC_LIBRARIES := 
+LOCAL_WHOLE_STATIC_LIBRARIES := 
+LOCAL_WHOLE_STATIC_LIBRARIES := 
